9dcbcca793a46a99da9f412a24653ec886db5371
Todor Mazgalov <todor.mazgalov@gmail.com>
2016-09-04 16:29:45 +0300

    Added comments

--- src/main/groovy/name/mazgalov/vaadin/sass/compiler/VaadinSassCompiler.groovy
+++ src/main/groovy/name/mazgalov/vaadin/sass/compiler/VaadinSassCompiler.groovy
@@ -4,6 +4,9 @@ import name.mazgalov.vaadin.sass.compiler.task.CompileSass
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+/**
+ * Created by Todor Mazgalov
+ */
 class VaadinSassCompiler implements Plugin<Project> {
     void apply(Project project) {
         project.tasks.create('compileSass', CompileSass.class)
